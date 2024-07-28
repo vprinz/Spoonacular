0d1d5d34b55a03554aa6f32baac43c93073fc231
Valerii Pavlikov <valerypavlikov@yandex.ru>
2024-07-28 11:39:26 +0300

    Initial Commit

---
+++ Spoonacular/SpoonacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoonacularApp.swift
+//  Spoonacular
+//
+//  Created by Valerie N. Prinz on 28/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpoonacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
